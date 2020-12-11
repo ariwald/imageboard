e0a84838807a47ea6c307ab80aedbada5f79d857
ariwald <arisouza@gmail.com>
2020-12-11 16:15:59 +0100

    uploadind pics

--- s3.js
+++ s3.js
@@ -3,42 +3,42 @@ const fs = require("fs");
 
 let secrets;
 if (process.env.NODE_ENV == "production") {
-    secrets = process.env;
+  secrets = process.env;
 } else {
-    secrets = require("./secrets");
+  secrets = require("./secrets");
 }
 
 const s3 = new aws.S3({
-    accessKeyId: secrets.AWS_KEY,
-    secretAccessKey: secrets.AWS_SECRET
+  accessKeyId: secrets.AWS_KEY,
+  secretAccessKey: secrets.AWS_SECRET
 });
 
 module.exports.upload = (req, res, next) => {
-    if (!req.file) {
-        console.log("req.file is not there");
-        return res.sendStatus(500);
-    }
-    const { filename, mimetype, size, path } = req.file;
-    s3.putObject({
-        //bucket name
-        Bucket: "spicedling",
-        //acces control
-        ACL: "public-read",
-        //req.file
-        Key: filename,
-        Body: fs.createReadStream(path),
-        ContentType: mimetype,
-        ContentLength: size
+  if (!req.file) {
+    console.log("req.file is not there");
+    return res.sendStatus(500);
+  }
+  const { filename, mimetype, size, path } = req.file;
+  s3.putObject({
+    //bucket name
+    Bucket: "ariawsgenaubucket",
+    //acces control
+    ACL: "public-read",
+    //req.file
+    Key: filename,
+    Body: fs.createReadStream(path),
+    ContentType: mimetype,
+    ContentLength: size
+  })
+    .promise()
+    .then(() => {
+      console.log("it worked...promise");
+      next();
+      //to delete the files
+      fs.unlink(path, () => {});
     })
-        .promise()
-        .then(() => {
-            console.log("it worked...promise");
-            next();
-            //to delete the files
-            fs.unlink(path, () => {});
-        })
-        .catch(err => {
-            console.log(err, "promise failure");
-            res.sendStatus(500);
-        });
+    .catch(err => {
+      console.log(err, "promise failure");
+      res.sendStatus(500);
+    });
 };
